07b7f64304fcfa09ae300b36cb2ecdeb1b8e5868
forkiesassds <icanttellyou@memeware.net>
2022-08-07 23:58:07 +0300

    zone block

--- TwentyWFourteenInfinite/src/main/java/me/zero/twentywfourteeninfinite/registry/ModBlocks.java
+++ TwentyWFourteenInfinite/src/main/java/me/zero/twentywfourteeninfinite/registry/ModBlocks.java
@@ -2,11 +2,14 @@ package me.zero.twentywfourteeninfinite.registry;
 
 import me.zero.aprilfools.api.registry.BlockRegistry;
 import me.zero.twentywfourteeninfinite.TwentyWFourteenInfiniteMod;
+import me.zero.twentywfourteeninfinite.mixin.InvokerBlocks;
 import me.zero.twentywfourteeninfinite.registry.block.AntBlock;
 import me.zero.twentywfourteeninfinite.registry.block.BoxOfBooksBlock;
 import me.zero.twentywfourteeninfinite.registry.block.NeitherPortalBlock;
+import me.zero.twentywfourteeninfinite.registry.block.ZoneBlock;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -16,12 +19,14 @@ import static net.minecraft.world.level.block.Blocks.NETHERITE_BLOCK;
 
 public class ModBlocks {
     public static final Block NEITHER_PORTAL = new NeitherPortalBlock(BlockBehaviour.Properties.of(Material.PORTAL).noCollission().strength(-1.0F).sound(SoundType.GLASS).lightLevel((cdt) -> 11).noLootTable());
+    public static final Block ZONE = new ZoneBlock(BlockBehaviour.Properties.of(Material.BARRIER).strength(-1.0F, 3600000.8F).noLootTable().noOcclusion().isValidSpawn(InvokerBlocks::invokeNever).isViewBlocking(InvokerBlocks::invokeAlways));
     public static final Block NETHERITE_STAIRS = new StairBlock(NETHERITE_BLOCK.defaultBlockState(), BlockBehaviour.Properties.copy(NETHERITE_BLOCK));
     public static final Block ANT_BLOCK = new AntBlock(BlockBehaviour.Properties.of(Material.HEAVY_METAL).sound(SoundType.WET_GRASS).strength(-1.0F, 3600000.0F).noLootTable());
     public static final Block BOOK_BLOCK = new BoxOfBooksBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(1.5F).sound(SoundType.WOOD));
 
     public static void initBlocks() {
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "neither_portal"), NEITHER_PORTAL);
+        BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "zone"), ZONE);
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "netherite_stairs"), NETHERITE_STAIRS);
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "ant"), ANT_BLOCK);
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "book_box"), BOOK_BLOCK);
