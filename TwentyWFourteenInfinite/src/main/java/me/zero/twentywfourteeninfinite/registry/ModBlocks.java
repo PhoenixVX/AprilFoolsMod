cf8fcaa0f10dfa892148f27fe17dca612c5d3267
forkiesassds <icanttellyou@memeware.net>
2022-08-08 00:10:14 +0300

    cursor block

--- TwentyWFourteenInfinite/src/main/java/me/zero/twentywfourteeninfinite/registry/ModBlocks.java
+++ TwentyWFourteenInfinite/src/main/java/me/zero/twentywfourteeninfinite/registry/ModBlocks.java
@@ -8,8 +8,8 @@ import me.zero.twentywfourteeninfinite.registry.block.BoxOfBooksBlock;
 import me.zero.twentywfourteeninfinite.registry.block.NeitherPortalBlock;
 import me.zero.twentywfourteeninfinite.registry.block.ZoneBlock;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -23,6 +23,7 @@ public class ModBlocks {
     public static final Block NETHERITE_STAIRS = new StairBlock(NETHERITE_BLOCK.defaultBlockState(), BlockBehaviour.Properties.copy(NETHERITE_BLOCK));
     public static final Block ANT_BLOCK = new AntBlock(BlockBehaviour.Properties.of(Material.HEAVY_METAL).sound(SoundType.WET_GRASS).strength(-1.0F, 3600000.0F).noLootTable());
     public static final Block BOOK_BLOCK = new BoxOfBooksBlock(BlockBehaviour.Properties.of(Material.WOOD).strength(1.5F).sound(SoundType.WOOD));
+    public static final Block CURSOR = new Block(BlockBehaviour.Properties.of(Material.STONE, DyeColor.GREEN).strength(1.8F));
 
     public static void initBlocks() {
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "neither_portal"), NEITHER_PORTAL);
@@ -30,5 +31,6 @@ public class ModBlocks {
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "netherite_stairs"), NETHERITE_STAIRS);
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "ant"), ANT_BLOCK);
         BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "book_box"), BOOK_BLOCK);
+        BlockRegistry.registerBlock(new ResourceLocation(TwentyWFourteenInfiniteMod.MOD_ID, "cursor"), CURSOR);
     }
 }
